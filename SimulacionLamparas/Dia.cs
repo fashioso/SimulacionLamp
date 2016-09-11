d5533d380260fee23626242152a01e16908f7927
DavidY <dfyanez@hotmail.com>
2016-09-11 00:08:01 -0700

    Primera publicacion del Proyecto

---
+++ SimulacionLamparas/Dia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionLamparas
+{
+
+    class Dia
+    {
+        static int horas = 24;
+        DateTime fecha;
+        int temperatura;
+
+
+        public Dia(DateTime fechaInic)
+        {
+
+        }
+
+
+    }
+
+    
+}
